4ea0c07523380a3ed9c12386b581ee0336e07be1
Mike J <junokasm@gmail.com>
2025-08-05 11:02:30 -0500

    Adding boilerplate synthesis, but realizing that learning is wrong, so fixing visible mean and appropriately normalizing visible and historic data as well as reducing learning rate to ensure convergence

--- Dissertation Refactor/fcrbm_utilities.py
+++ Dissertation Refactor/fcrbm_utilities.py
@@ -36,11 +36,19 @@ class FCRBM(tf.Module):
         
         return tf.sigmoid(energy_term + dynamic_hidden_bias)
 
+    # @tf.function
+    # def _visible_mean(self, h, u):
+    #     # calculate dynamic visible biases
+    #     dynamic_visible_bias = self.visible_bias + tf.linalg.matmul(h, self.W_vh, transpose_b=True)
+    #     return dynamic_visible_bias # mean for a gaussian visible layer
+
     @tf.function
     def _visible_mean(self, h, u):
         # calculate dynamic visible biases
-        dynamic_visible_bias = self.visible_bias + tf.linalg.matmul(h, self.W_vh, transpose_b=True)
-        return dynamic_visible_bias # mean for a gaussian visible layer
+        dynamic_visible_bias = self.visible_bias + tf.linalg.matmul(u, self.W_vu, transpose_b=True)
+        # The reconstruction term from the hidden units
+        reconstruction_term = tf.linalg.matmul(h, self.W_vh, transpose_b=True)
+        return dynamic_visible_bias + reconstruction_term # mean for a gaussian visible layer
 
     @tf.function
     def gibbs_sampling_k_steps(self, v_init, u, y):
@@ -54,63 +62,46 @@ class FCRBM(tf.Module):
             
         return v_current, h_sample
 
+        
     @tf.function
     def train_step(self, v_pos, u, y, optimizer):
-        with tf.GradientTape() as tape:
-            # --- Positive Phase ---
-            h_pos_prob = self._hidden_prob(v_pos, u, y)
-            # sample hidden units from the probabilities
-            h_pos_sample = tf.cast(tf.random.uniform(tf.shape(h_pos_prob)) < h_pos_prob, tf.float32)
-
-            # positive phase association term
-            # this is the "ideal" state of the model.
-            pos_v_h = tf.linalg.matmul(v_pos, h_pos_prob, transpose_a=True)
-            pos_v_u = tf.linalg.matmul(v_pos, u, transpose_a=True)
-            pos_h_u = tf.linalg.matmul(h_pos_prob, u, transpose_a=True)
-            
-            # --- Negative Phase ---
-            v_neg, h_neg_sample = self.gibbs_sampling_k_steps(v_pos, u, y)
-
-            # negative phase association term
-            # this is the "undesirable" state of the model.
-            neg_v_h = tf.linalg.matmul(v_neg, h_neg_sample, transpose_a=True)
-            neg_v_u = tf.linalg.matmul(v_neg, u, transpose_a=True)
-            neg_h_u = tf.linalg.matmul(h_neg_sample, u, transpose_a=True)
-            
-            # --- Loss Calculation ---
-            # The "loss" is the difference between the energy terms of the positive and negative phases
-            # reconstruction error as proxy
-            # for an RBM, the gradient is important so compute the gradients based on the difference of associationsdirectly.
-            
-            # The "loss" for the tape is not a single value but a set of updates
-            # that is manually applied. This is a common pattern for RBMs.
-            
-            # Calculate the gradient for each parameter based on the difference of associations
-            # Note: The gradient for W_vh_style is more complex due to the einsum.
-            grad_W_vh = (pos_v_h - neg_v_h)
-            grad_W_vu = (pos_v_u - neg_v_u)
-            grad_W_hu = (pos_h_u - neg_h_u)
-            grad_visible_bias = tf.reduce_mean(v_pos - v_neg, axis=0)
-            grad_hidden_bias = tf.reduce_mean(h_pos_prob - h_neg_sample, axis=0)
-            
-            # The gradient for the three-way interaction term is more complex.
-            # We need to compute the association for each style dimension.
-            pos_association_style = tf.einsum('bv,bh,bs->vhs', v_pos, h_pos_prob, y)
-            neg_association_style = tf.einsum('bv,bh,bs->vhs', v_neg, h_neg_sample, y)
-            grad_W_vh_style = pos_association_style - neg_association_style
-            
-            # Collect all gradients
-            gradients = [grad_W_vh, grad_W_vu, grad_W_hu, grad_visible_bias, grad_hidden_bias, grad_W_vh_style]
-            
+        # --- Positive Phase ---
+        h_pos_prob = self._hidden_prob(v_pos, u, y)
+        h_pos_sample = tf.cast(tf.random.uniform(tf.shape(h_pos_prob)) < h_pos_prob, tf.float32)
+    
+        # positive phase association term
+        pos_v_h = tf.linalg.matmul(v_pos, h_pos_prob, transpose_a=True)
+        pos_v_u = tf.linalg.matmul(v_pos, u, transpose_a=True)
+        pos_h_u = tf.linalg.matmul(h_pos_prob, u, transpose_a=True)
+        
+        # --- Negative Phase ---
+        v_neg, h_neg_sample = self.gibbs_sampling_k_steps(v_pos, u, y)
+    
+        # negative phase association term
+        neg_v_h = tf.linalg.matmul(v_neg, h_neg_sample, transpose_a=True)
+        neg_v_u = tf.linalg.matmul(v_neg, u, transpose_a=True)
+        neg_h_u = tf.linalg.matmul(h_neg_sample, u, transpose_a=True)
+        
+        # --- Manual Gradient Calculation ---
+        grad_W_vh = (pos_v_h - neg_v_h)
+        grad_W_vu = (pos_v_u - neg_v_u)
+        grad_W_hu = (pos_h_u - neg_h_u)
+        grad_visible_bias = tf.reduce_mean(v_pos - v_neg, axis=0)
+        grad_hidden_bias = tf.reduce_mean(h_pos_prob - h_neg_sample, axis=0)
+        
+        pos_association_style = tf.einsum('bv,bh,bs->vhs', v_pos, h_pos_prob, y)
+        neg_association_style = tf.einsum('bv,bh,bs->vhs', v_neg, h_neg_sample, y)
+        grad_W_vh_style = pos_association_style - neg_association_style
+        
+        gradients = [grad_W_vh, grad_W_vu, grad_W_hu, grad_visible_bias, grad_hidden_bias, grad_W_vh_style]
+        
         # Manually apply gradients
-        # The optimizer takes care of learning rate and momentum
         trainable_vars = [self.W_vh, self.W_vu, self.W_hu, self.visible_bias, self.hidden_bias, self.W_vh_style]
         optimizer.apply_gradients(zip(gradients, trainable_vars))
         
-        # return a value for logging purposes, e.g., reconstruction error
         reconstruction_error = tf.reduce_mean(tf.square(v_pos - v_neg))
         return reconstruction_error
-
+        
 def synthesize_audio(model, u_seed, y_seed, num_frames_to_generate, n_fft, hop_length, sr):
     """
     Synthesizes a new audio spectrogram using the trained FCRBM.
